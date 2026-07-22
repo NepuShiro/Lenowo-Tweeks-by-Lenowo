f82e9d2d9bce1222c050c1994a730071d6e603b8
IHaveAName2653 <89300269+IHaveAName2653@users.noreply.github.com>
2026-07-22 13:38:29 +1000

    Fix AssemblyTitle/Product not being set properly
    Somehow forgot to set this for the rest of the projects, so they are now properly set.

--- LenowoTweeks.ProtoFlux/Properties/AssemblyInfo.cs
+++ LenowoTweeks.ProtoFlux/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("LenowoTweeks")]
-[assembly: AssemblyProduct("LenowoTweeks")]
+[assembly: AssemblyTitle("LenowoTweeks.ProtoFlux")]
+[assembly: AssemblyProduct("LenowoTweeks.ProtoFlux")]
 [assembly: AssemblyDescription("A Mod made by Rosa to tweek things I don't like")]
 [assembly: AssemblyCompany("Rosa")]
 [assembly: AssemblyCopyright("Copyright © 2026 Rosa")]
